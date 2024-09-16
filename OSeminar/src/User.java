2b9d6a790b661360b1068ef3eedda1fedf198e60
ProkopenkoIA <boroda5384@mail.ru>
2024-09-16 09:38:30 +0500

    Обобщения - Учителя

---
+++ OSeminar/src/User.java
@@ -0,0 +1,29 @@
+public class User {
+    private String firstName;
+    private String lastName;
+    private String midleName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getMidleName() {
+        return midleName;
+    }
+
+    public void setMidleName(String midleName) {
+        this.midleName = midleName;
+    }
+}
